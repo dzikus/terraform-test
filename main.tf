bf7d15e88633e806ff0ab5489e51fdeab8b54caa
dzikus99@gmail.com <grzegorz.sterniczuk@gmail.com>
2022-07-26 09:45:43 +0200

    add variable.tf and data for amazon ami

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 #Remote Backend Configuration
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "dzikus"
     workspaces {
-        name = "workspace"
+      name = "workspace"
     }
   }
 }
@@ -25,3 +25,22 @@ provider "aws" {
 #  ami = "ami-0a1ee2fb28fe05df3"
 #  instance_type = "t2.micro"
 #}
+
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
